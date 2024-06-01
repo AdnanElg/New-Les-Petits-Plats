29f329e8381015dbf7226ded56c4bfa6d6f4b79c
El Ghalbzouri Adnan <elghalbzouriadnan@OrdinatabledeEl.lan>
2024-06-01 23:59:26 +0200

    fix

--- src/utils/launchSearch.ts
+++ src/utils/launchSearch.ts
@@ -1,87 +1,70 @@
 // Import des modules
 import { recipes } from "../data/recipes.ts";
 import { cards } from "../components/cards.ts";
+import { CardType } from "../types/cards.types.ts";
 
 export const launchSearch = () => {
   // Récupération des tags sélectionnés de la section :
   const tagsList = document.querySelectorAll("#lists-tag");
-  const selectedTagsList = Array.from(tagsList, (tag) => tag.textContent);
+  const selectedTagsList = Array.from(
+    tagsList,
+    (tag) => tag.textContent?.trim().toLowerCase() || ""
+  );
 
   // Récupération de la valeur saisie dans l'input de recherche
-  var searchInput = document.querySelector("#search-input") as HTMLInputElement;
-  if (searchInput) {
-    var searchTerm = searchInput.value.toLowerCase();
-    console.log(searchTerm);
-  }
+  const searchInput = document.querySelector(
+    "#search-input"
+  ) as HTMLInputElement;
+  const searchTerm = searchInput ? searchInput.value.toLowerCase() : "";
 
   // Tableau pour stocker les recettes trouvées
-  var foundRecipes = [];
+  const foundRecipes = [];
 
   // Parcours des recettes pour filtrer en fonction des critères sélectionnés
   for (let i = 0; i < recipes.length; i++) {
     const recipe = recipes[i];
 
     // Récupération des ingrédients de la recette
-    const recipeIngredients = [];
-    for (let i = 0; i < recipe.ingredients.length; i++) {
-      const ingredient = recipe.ingredients[i];
-      if (ingredient && ingredient.ingredient) {
-        recipeIngredients.push(ingredient.ingredient.toLowerCase());
-      }
-    }
+    const recipeIngredients = recipe.ingredients.map((ingredient) =>
+      ingredient.ingredient.toLowerCase()
+    );
 
     // Récupération des ustensiles de la recette
-    const recipeUstensils = [];
-    for (let i = 0; i < recipe.ustensils.length; i++) {
-      const ustensil = recipe.ustensils[i];
-      if (ustensil) {
-        recipeUstensils.push(ustensil.toLowerCase());
-      }
-    }
+    const recipeUstensils = recipe.ustensils.map((ustensil) =>
+      ustensil.toLowerCase()
+    );
 
     // Variables pour les contrôles des critères de filtrage
-    let checkUstensil = true;
-    let checkDevice = true;
-    let checkIngredient = true;
+    let checkUstensils = true;
+    let checkAppliances = true;
+    let checkIngredients = true;
     let checkSearch = true;
 
     // Vérification des tags d'ingrédients sélectionnés
-    if (selectedTagsList.length > 0) {
-      let i = 0;
-      while (i < selectedTagsList.length) {
-        let tag = selectedTagsList[i].trim();
-        if (!recipeIngredients.includes(tag.toLowerCase())) {
-          checkIngredient = false;
-        }
-        i++;
+    for (const tag of selectedTagsList) {
+      if (tag && !recipeIngredients.includes(tag)) {
+        checkIngredients = false;
+        break;
       }
     }
 
-    // //? Vérification des tags d'appareils sélectionnés
-    if (selectedTagsList.length > 0) {
-      let i = 0;
-      while (i < selectedTagsList.length) {
-        let tag = selectedTagsList[i].trim();
-        if (!recipe.appliance.toLowerCase().includes(tag.toLowerCase())) {
-          checkDevice = false;
-        }
-        i++;
+    // Vérification des tags d'appareils sélectionnés
+    for (const tag of selectedTagsList) {
+      if (tag && !recipe.appliance.toLowerCase().includes(tag)) {
+        checkAppliances = false;
+        break;
       }
     }
 
-    //? Vérification des tags d'ustensiles sélectionnés
-    if (selectedTagsList.length > 0) {
-      let i = 0;
-      while (i < selectedTagsList.length) {
-        let tag = selectedTagsList[i].trim();
-        if (!recipeUstensils.includes(tag.toLowerCase())) {
-          checkUstensil = false;
-        }
-        i++;
+    // Vérification des tags d'ustensiles sélectionnés
+    for (const tag of selectedTagsList) {
+      if (tag && !recipeUstensils.includes(tag)) {
+        checkUstensils = false;
+        break;
       }
     }
 
-    //? Vérification du critère de recherche textuelle
+    // Vérification du critère de recherche textuelle
     if (searchTerm.length >= 3) {
       if (
         !recipe.name.toLowerCase().includes(searchTerm) &&
@@ -92,9 +75,28 @@ export const launchSearch = () => {
       }
     }
 
-    //? Ajout de la recette au tableau des recettes trouvées
-    if (checkUstensil && checkDevice && checkIngredient && checkSearch) {
-      foundRecipes.push(recipe);
+    // Ajout de la recette au tableau des recettes trouvées
+    if (checkUstensils && checkAppliances && checkIngredients && checkSearch) {
+      foundRecipes.push(recipe as CardType);
+    }
+  }
+
+  // Récupération de la section des cartes
+  const cardsSection = document.querySelector("#section-3 .row");
+
+  // Affichage des résultats ou du message d'aucune recette trouvée
+  if (cardsSection) {
+    if (foundRecipes.length === 0) {
+      cardsSection.innerHTML = `
+        <p class="text-center display-6">
+          Désolé, aucune recette ne correspond à votre recherche pour <span class='fw-bold'>${searchTerm}</span>. 
+          Veuillez essayer avec d'autres mots-clés :(
+        </p>`;
+    } else {
+      cardsSection.innerHTML = "";
+      foundRecipes.forEach((recipe) => {
+        cardsSection.append(cards(recipe));
+      });
     }
   }
 };
